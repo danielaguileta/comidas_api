f6577e77ecabd85096403ff008933e9719929880
danielaguileta10 <daniel.aguileta@unah.hn>
2022-12-06 12:07:27 -0600

    server.jsç

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const dbOptions = {
     host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || "3306",
     user: process.env.DB_USER || "root",
+    database: process.env.DB_NAME || "oficial_noysita",
     password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "oficial_noysita"
 }
 
 // Middlewares
